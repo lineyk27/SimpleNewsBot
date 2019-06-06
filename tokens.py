de0ee03eba7c4eaff4d5ab905482309f22d009a4
lineyk27 <lineyk27gg@gmail.com>
2019-06-06 14:31:16 +0300

    Changed interaction with db, from postgresql to mysql

---
+++ tokens.py
@@ -0,0 +1,13 @@
+TOKEN_NEWS = 'a09b551fc69748b0a7567c7bb7af976f'
+TOKEN_BOT = '826689167:AAE7R_ScZlR7aSxTK_GvrqVJvgLEtHC4NDk'
+DB_CONFIG = {
+    'host': '127.0.0.1',
+    'user': 'lineyk27',
+    'database': 'lineyk27@simplenews',
+    'password': 'pass-dd1y4d'
+}
+SSH_CONFIG = {
+    'username': 'lineyk27',
+    'password': 'as2mo5dot183',
+    'remote': ('lineyk27.mysql.pythonanywhere-services.com', 3306)
+    }
